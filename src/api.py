fc5aa3a97b3d68860b8bf2bdf1afd828b90a8331
allan_horst <allanhorstwillig@gmail.com>
2018-08-13 21:31:51 -0300

    Removed person test

--- src/api.py
+++ src/api.py
@@ -1,18 +1,14 @@
 import flask
-from person import Person
 import names
 import json
+import cv2
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-  personList = []
-  for i in range(0, 5):
-    personList.append(Person(names.get_first_name(), names.get_last_name()))
-    jsonStr = json.dumps([p.toJSON() for p in personList ])
 
-  return jsonStr
+  return ""
 
 app.run()
