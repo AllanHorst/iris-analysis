1743882510b667b3b01c1772123bbba8955f863b
allan_horst <allanhorstwillig@gmail.com>
2018-08-23 20:43:23 -0300

    Contrast working

--- src/process_image.py
+++ src/process_image.py
@@ -13,7 +13,7 @@ def check_color(px, my_range, my_list):
   min_value = my_list[0]
   max_value = my_list[my_range -1]
   if (min_value >= px and px <= max_value):
-    return max_value / 2
+    return max_value
 
   return None
 
@@ -22,20 +22,22 @@ def contrast(gray, my_range):
 
   for i in range(0, len(gray)):
     for j in range(0, len(gray[i])):
+
       for k in range(0, len(ranges)):
         new_color = check_color(gray[i][j], my_range, ranges[k])
         if (new_color):
           gray[i][j] = new_color
-          continue
+          break
 
   return gray
 
-img = cv2.imread('images/with_diabetes/6esim.png')
+img = cv2.imread('images/with_diabetes/3esim.png')
 img = cv2.resize(img, (720, 576))
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+original_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-np.savetxt('text1.txt',gray,fmt='%.2f')
-gray = contrast(gray, 16)
+cv2.imshow('original_gray', original_gray)
+np.savetxt('text1.txt',original_gray,fmt='%.2f')
+gray = contrast(original_gray, 16)
 np.savetxt('text2.txt',gray,fmt='%.2f')
 cv2.imshow('th1', gray)
 
