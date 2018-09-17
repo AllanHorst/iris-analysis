a88db594c496b5a97b9b839f9c54444a4876fedd
allan_horst <allanhorstwillig@gmail.com>
2018-09-17 01:17:57 -0300

    criado processo de analise de imagem

--- src/analysis.py
+++ src/analysis.py
@@ -1,7 +1,46 @@
 import numpy
 import cv2
 
+colors = {}
+predominant = None
+image = None
+
+def add_predominant(pixel):
+  global predominant
+  count = colors[str(pixel)]
+
+  if (predominant != None and predominant['pixel'] == pixel):
+    predominant['count'] = count
+    return
+
+  if (predominant == None or predominant['count'] < count ):
+    predominant = {
+      'pixel': pixel,
+      'count': count
+    }
+
+def add_color(pixel):
+  global colors
+  count = colors[str(pixel)] if str(pixel) in colors else 0
+  colors[str(pixel)] = count + 1
+
+def process(i, j):
+  pixel = image[i][j]
+  add_color(pixel)
+  add_predominant(pixel)
+
 def apply(img):
+  global image
+  image = img
+  print(len(img))
+  print(len(img[0]))
+  soma = 0
   for i in range(0, len(img)):
     for j in range(0, len(img[i])):
-      print(img[i][j])
+      process(i, j)
+      soma += 1
+
+  print(soma)
+  print(colors)
+  print('*' * 50)
+  print(predominant)
