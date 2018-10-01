7a01c1a1a58ecc53281b1e2a84dbf8bf6f4989d2
allan_horst <allanhorstwillig@gmail.com>
2018-10-01 10:46:26 -0300

    Ajustado api para receber parametro de imagem e abrir com opencv

--- src/api.py
+++ src/api.py
@@ -1,14 +1,20 @@
-import flask
+from flask import Flask, request
 import names
 import json
 import cv2
+import numpy as np
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET'])
-def home():
-
-  return ""
+@app.route('/analyze', methods=['POST'])
+def analyze():
+  r = request
+  print(r)
+  nparr = np.fromstring(r.data, np.uint8)
+  # decode image
+  img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+  print(img)
+  return "show"
 
 app.run()
