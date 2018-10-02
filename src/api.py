b17cac891dd075dc9f57bfdf3ac463adb4d6f628
allan_horst <allanhorstwillig@gmail.com>
2018-10-02 09:03:42 -0300

    Ajustado api

--- src/api.py
+++ src/api.py
@@ -1,20 +1,20 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import names
 import json
 import cv2
 import numpy as np
 from process_image import process
 
-
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 app.config["DEBUG"] = True
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/api/analyze', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def analyze():
   r = request
-  print(r)
   nparr = np.fromstring(r.data, np.uint8)
-  # decode image
   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
   process(img)
   return "show"
