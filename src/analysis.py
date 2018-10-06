0054f9468e1da154a88ea4b6dc41b7af2bf95864
allan_horst <allanhorstwillig@gmail.com>
2018-10-06 15:31:19 -0300

    Commit for safe

--- src/analysis.py
+++ src/analysis.py
@@ -1,23 +1,62 @@
 import numpy as np
 import cv2
-import contrast
+
+def find_smudge(img, y, x):
+  smudge = []
+  for i2 in range(y, len(img)):
+    found = False
+    for j2 in range(x, len(img[y])):
+      color = img[i2][j2]
+      if (color == 255):
+        continue
+
+      found = True
+      smudge.append([i2, j2])
+
+    if (not found):
+      break
+
+  return smudge
+
+def founded(i, j, smudges):
+  for k in range(0, len(smudges)):
+    for l in range(0, len(smudges[k])):
+      if (smudges[k][l][0] == i and smudges[k][l][1] == j):
+        return True
+  return False
 
 def analyze(img):
-  print(img)
   th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY, 31,2)
-  # contrast.apply(img, 8)
-  hist = cv2.calcHist([img],[0],None,[256],[0,255])
+            cv2.THRESH_BINARY, 61,10)
+  blank = np.zeros((img.shape))
+  blank.fill(255)
+  smudges = []
+  black = 0
+  white = 0
+  for y in range(0, len(img)):
+    for x in range(0, len(img[y])):
 
-  blank = np.zeros((256, 256))
+      if (th3[y][x] == 255):
+        continue
+      color = img[y][x]
+      if (color < 120):
+        blank[y][x] = 0
 
-  for x,y in enumerate(hist):
-    print(x, y)
-    cv2.line(blank, (x, 0), (x, y), (255, 255, 255))
+  # for i in range(0, len(smudges)):
+  #   smudge = smudges[i]
+  #   for j in range(0, len(smudge)):
+  #     y = smudge[j][0]
+  #     x = smudge[j][1]
+  #     color = img[y][x]
+  #     if (color < 120):
+  #       blank[y][x] = 0
 
-  cv2.imshow('a', blank)
+      # blank[y][x] = 0
 
-  cv2.imshow('or', img)
-  # cv2.imshow('img', th3)
+    # if (smudge)
+
+  # cv2.imshow('img', img)
+  cv2.imshow('or', th3)
+  cv2.imshow('blank', blank)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
--- src/normalize.py
+++ src/normalize.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def four_pieces(imgs, radius):
+def four_pieces(imgs):
   functions = { "0": first, "1": second, "2": third, "3": fourth }
   pieces = []
   for i in range(0, len(imgs)):
@@ -19,26 +19,26 @@ def first(img):
   rows,cols = img.shape
   M = cv2.getRotationMatrix2D((cols/2,rows/2), 45, 1)
   dst = cv2.warpAffine(img, M, (cols,rows))
-  croped_img = dst[30:60, 35:85]
+  croped_img = dst[30:60, 35:75]
   return croped_img
 
 def second(img):
   rows,cols = img.shape
   M = cv2.getRotationMatrix2D((cols/2,rows/2), -45, 1)
   dst = cv2.warpAffine(img,M,(cols,rows))
-  croped_img = dst[30:60, 19:78]
+  croped_img = dst[30:60, 25:78]
   return croped_img
 
 def third(img):
   rows,cols = img.shape
   M = cv2.getRotationMatrix2D((cols/2,rows/2), -45, 1)
   dst = cv2.warpAffine(img,M,(cols,rows))
-  croped_img = dst[30:60, 35:85]
+  croped_img = dst[30:60, 35:75]
   return croped_img
 
 def fourth(img):
   rows,cols = img.shape
   M = cv2.getRotationMatrix2D((cols/2,rows/2), 45, 1)
   dst = cv2.warpAffine(img,M,(cols,rows))
-  croped_img = dst[30:60, 19:78]
+  croped_img = dst[30:60, 25:78]
   return croped_img
