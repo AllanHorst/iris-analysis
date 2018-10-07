7f589e82a5849ec559922ff9e48d62981b280ac1
allan_horst <allanhorstwillig@gmail.com>
2018-10-07 20:28:01 -0300

    Mais ajustes

--- src/analysis.py
+++ src/analysis.py
@@ -1,62 +1,42 @@
 import numpy as np
 import cv2
 
-def find_smudge(img, y, x):
-  smudge = []
-  for i2 in range(y, len(img)):
-    found = False
-    for j2 in range(x, len(img[y])):
-      color = img[i2][j2]
-      if (color == 255):
-        continue
+min_area = 100
+max_area = 800
 
-      found = True
-      smudge.append([i2, j2])
-
-    if (not found):
-      break
-
-  return smudge
-
-def founded(i, j, smudges):
-  for k in range(0, len(smudges)):
-    for l in range(0, len(smudges[k])):
-      if (smudges[k][l][0] == i and smudges[k][l][1] == j):
-        return True
-  return False
+def draw_countour(img, cnt):
+  rect = cv2.minAreaRect(cnt)
+  box = cv2.boxPoints(rect)
+  box = np.int0(box)
+  cv2.drawContours(img,[box],0,(0,0,255),1)
 
 def analyze(img):
+  has_smudge = False
   th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY, 61,10)
-  blank = np.zeros((img.shape))
-  blank.fill(255)
-  smudges = []
-  black = 0
-  white = 0
+            cv2.THRESH_BINARY_INV, 41,3)
+
   for y in range(0, len(img)):
     for x in range(0, len(img[y])):
-
-      if (th3[y][x] == 255):
+      if (th3[y][x] == 0):
         continue
-      color = img[y][x]
-      if (color < 120):
-        blank[y][x] = 0
+      if (img[y][x] < 100):
+        th3[y][x] = 255
 
-  # for i in range(0, len(smudges)):
-  #   smudge = smudges[i]
-  #   for j in range(0, len(smudge)):
-  #     y = smudge[j][0]
-  #     x = smudge[j][1]
-  #     color = img[y][x]
-  #     if (color < 120):
-  #       blank[y][x] = 0
 
-      # blank[y][x] = 0
+  im2, contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  for i in range(0, len(contours)):
+    cnt = contours[i]
+    area = cv2.contourArea(cnt)
+    print(area)
+    if (area > min_area and area < max_area):
+      has_smudge = True
+      break
 
-    # if (smudge)
 
+  print(has_smudge)
+  return has_smudge
+
+  # cv2.imshow('th3', th3)
   # cv2.imshow('img', img)
-  cv2.imshow('or', th3)
-  cv2.imshow('blank', blank)
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
--- src/process_image.py
+++ src/process_image.py
@@ -8,18 +8,23 @@ import normalize
 def process(img):
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   imgs = pupil_finder.find(img)
+  smudges = 0
   if (not imgs or len(imgs) != 4):
     print('Pupil not founded')
   else:
     pieces = normalize.four_pieces(imgs)
     for i in range(0, len(pieces)):
-      analysis.analyze(pieces[i])
+      if (analysis.analyze(pieces[i])):
+        smudges += 1
 
+  return smudges == 4
 
-# img = cv2.imread('images/without_diabetes/3d.png', 0)
+# img = cv2.imread('images/without_diabetes/3d.png')
+img = cv2.imread('images/with_diabetes/5esim.png')
 img = cv2.imread('images/with_diabetes/4dsim.png')
 # cv2.imshow('img', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 # img = cv2.imread('images/without_diabetes_with_background/3e.png')
-process(img)
+result = process(img)
+print('result: ', result)
