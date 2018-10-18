8e75591c108806cd956e5dc08dd6b205472fa761
allan_horst <allanhorstwillig@gmail.com>
2018-10-18 13:41:41 -0300

    fuzzify

--- src/analysis.py
+++ src/analysis.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 
-min_area = 100
-max_area = 800
+min_area = 50
+medium_area = 300
+max_area = 600
 
 def draw_countour(img, cnt):
   rect = cv2.minAreaRect(cnt)
@@ -10,28 +11,28 @@ def draw_countour(img, cnt):
   box = np.int0(box)
   cv2.drawContours(img,[box],0,(0,0,255),1)
 
-def analyze(img):
-  has_smudge = False
-  th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY_INV, 41,5)
+def fuzzify(avarage):
+  calc1 = (avarage - min_area) / (medium_area - min_area)
+  calc2 = (max_area - avarage) / (max_area - medium_area)
+  return max(min(calc1, calc2), 0)
 
-  for y in range(0, len(img)):
-    for x in range(0, len(img[y])):
-      if (th3[y][x] == 0):
-        continue
-      print(img[y][x])
-      if (img[y][x] > 150):
-        th3[y][x] = 255
+def analyze(img):
+  th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY_INV, 41, 4)
 
 
   im2, contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  sum_area = 0
+  count = 0
   for i in range(0, len(contours)):
     cnt = contours[i]
     area = cv2.contourArea(cnt)
-    print(area)
     if (area > min_area and area < max_area):
+      sum_area += area
+      count += 1
       draw_countour(img, cnt)
-      has_smudge = True
       break
 
-  return has_smudge
+  avarage = sum_area / count
+  return fuzzify(avarage), img
+
--- src/process_image.py
+++ src/process_image.py
@@ -7,24 +7,22 @@ import normalize
 
 def process(img):
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  # img = cv2.resize(img,  (300, 300))
   imgs = pupil_finder.find(img)
-  smudges = 0
   if (not imgs or len(imgs) != 4):
     print('Pupil not founded')
   else:
     pieces = normalize.four_pieces(imgs)
     for i in range(0, len(pieces)):
-      if (analysis.analyze(pieces[i])):
-        smudges += 1
-
-  return smudges == 4
+      result = analysis.analyze(pieces[i])
+      print(result)
 
 img = cv2.imread('images/without_diabetes/5d.png')
 img = cv2.imread('images/with_diabetes/5esim.png')
-img = cv2.imread('images/with_diabetes/4dsim.png')
+# img = cv2.imread('images/with_diabetes/4dsim.png')
+# img = cv2.imread('images/without_diabetes_with_background/3e.png')
 # cv2.imshow('img', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-# img = cv2.imread('images/without_diabetes_with_background/3e.png')
 result = process(img)
 print('result: ', result)
