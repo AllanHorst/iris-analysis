765c2c3c4bfe3fff716b5f082fdc355b01ce1c0b
allan_horst <allanhorstwillig@gmail.com>
2018-10-26 19:58:24 -0200

    Ajustado análise, agora sim

--- src/analysis.py
+++ src/analysis.py
@@ -2,8 +2,11 @@ import numpy as np
 import cv2
 
 min_area = 50
-medium_area = 300
-max_area = 600
+max_area = 800
+a = 50
+b = 250
+c = 500
+d = 800
 
 def draw_countour(img, cnt):
   rect = cv2.minAreaRect(cnt)
@@ -12,14 +15,15 @@ def draw_countour(img, cnt):
   cv2.drawContours(img,[box],0,(0,0,255),1)
 
 def fuzzify(avarage):
-  calc1 = (avarage - min_area) / (medium_area - min_area)
-  calc2 = (max_area - avarage) / (max_area - medium_area)
-  result = max(min(calc1, calc2), 0)
+  calc1 = (avarage - a) / (b - a)
+  calc2 = (d - avarage) / (d - c)
+  result = max(min(calc1, 1, calc2), 0)
   return result * 100
 
 def analyze(img):
+  print('<>' * 20)
   th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY_INV, 41, 4)
+            cv2.THRESH_BINARY_INV, 41, 3)
 
 
   im2, contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,11 +32,20 @@ def analyze(img):
   for i in range(0, len(contours)):
     cnt = contours[i]
     area = cv2.contourArea(cnt)
+    print('area: ', area)
     if (area > min_area and area < max_area):
       sum_area += area
       count += 1
       draw_countour(img, cnt)
       break
+
   avarage = sum_area / count
+  print('sum area: ', sum_area)
+  print('fuzzy result: ', fuzzify(avarage))
+  print('contours: ', len(contours))
+  cv2.imshow('image', img)
+  cv2.imshow('th3', th3)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
   return fuzzify(avarage), img
 
